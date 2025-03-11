a85a0b7c61d17b3c84a607b3a2fb2af3803ffdf2
NhanHTr <126033635+NhanHTr@users.noreply.github.com>
2025-03-11 09:37:16 +0700

    idk
    Buttons added

---
+++ at4 digi unity/Assets/Script/ScreenSwitching.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ScreenSwitching : MonoBehaviour
+{
+       public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    } 
+}
+
